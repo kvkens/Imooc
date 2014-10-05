c88dc1d69eb1c7c8be9ab8199152e32a7eeccfc4
Kvkens <kvkens@qq.com>
2014-10-06 00:11:20 +0800

    add node.js files

---
+++ app.js
@@ -0,0 +1,128 @@
+/*(c) Copyright 2014 Kvkens. All Rights Reserved. */
+var express = require("express");
+var path = require("path");
+var mongoose = require("mongoose");
+var port = process.env.PORT || 3000;
+var app = express();
+
+mongoose.connect("mongodb://localhost/imooc");
+var _ = require("underscore");
+var Movie = require("./models/movie");
+app.set("views", "./views/pages");
+app.set("view engine", "jade");
+//app.use(express.bodyParser());
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,"./public")));
+app.locals.moment = require("moment");
+app.listen(port);
+
+console.log("imooc started on port " + port);
+
+// index page
+app.get("/", function(req, res) {
+	Movie.fetch(function(err,movies){
+		if(err){
+			console.log(err);
+		}
+		res.render("index", {
+			title: "imooc 首页",
+			movies : movies
+		});
+	});
+
+});
+
+// detail page
+app.get("/movie/:id", function(req, res) {
+	var id = req.params.id;
+	Movie.findById(id,function(err,movie){
+		res.render("detail", {
+			title: "imooc:" + movie.title,
+			movie:movie
+		});
+	});
+});
+
+// admin page
+app.get("/admin/movie", function(req, res) {
+	res.render("admin", {
+		title: "imooc 后台录入页",
+		movie:{
+			doctor:'',
+			country:'',
+			title:'',
+			year:'',
+			poster:'',
+			language:'',
+			flash:'',
+			summary:''
+		}
+	});
+});
+app.get('/admin/update/:id',function(req,res){
+	var id = req.params.id;
+
+	if(id){
+		Movie.findById(id,function(err,movie){
+			res.render('admin',{
+				title:'imooc 后台更新页面 --- ' + movie.title,
+				movie:movie
+			});
+		});
+	}
+});
+// admin post movie
+app.post("/admin/movie/new",function(req,res){
+	var id = req.body.movie._id;
+	console.log(id);
+	var movieObj = req.body.movie;
+	var _movie;
+	console.log(movieObj);
+	if(id !== "undefined"){
+		Movie.findById(id,function(err,movie){
+			if(err){
+				console.log(err);
+			}
+
+			_movie = _.extend(movie,movieObj);
+			_movie.save(function(err,movie){
+				if(err){
+					console.log(err);
+				}
+				res.redirect("/movie/" + movie._id);
+			});
+		});
+	}else{
+		_movie = new Movie({
+			doctor : movieObj.doctor,
+			title : movieObj.title,
+			country : movieObj.country,
+			language : movieObj.language,
+			year : movieObj.year,
+			poster : movieObj.poster,
+			summary : movieObj.summary,
+			flash : movieObj.flash
+		});
+
+		_movie.save(function(err,movie){
+			if(err){
+					console.log(err);
+				}
+			res.redirect("/movie/" + movie._id);
+		});
+	}
+});
+
+// list page
+app.get("/admin/list", function(req, res) {
+	Movie.fetch(function(err,movies){
+		if(err){
+			console.log(err)
+		}
+		res.render('list',{
+			title:'imooc 列表页',
+			movies:movies
+		})
+	})
+});
