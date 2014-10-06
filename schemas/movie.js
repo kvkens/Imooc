e6a8611c1bc162a589807ccbcb9c2e7752477e90
Kvkens <kvkens@qq.com>
2014-10-07 00:39:07 +0800

    add UserSchemas User Module

--- schemas/movie.js
+++ schemas/movie.js
@@ -1,45 +1,47 @@
 var mongoose = require("mongoose");
 
 var MovieSchema = new mongoose.Schema({
-	doctor:String,
-	title:String,
-	language:String,
-	country:String,
-	summary:String,
-	flash:String,
-	poster:String,
-	year:String,
-	meta:{
-		createAt:{
-			type:Date,
-			default:Date.now()
+	doctor: String,
+	title: String,
+	language: String,
+	country: String,
+	summary: String,
+	flash: String,
+	poster: String,
+	year: String,
+	meta: {
+		createAt: {
+			type: Date,
+			default: Date.now()
 		},
-		updateAt:{
-			type:Date,
-			default:Date.now()
+		updateAt: {
+			type: Date,
+			default: Date.now()
 		}
 	}
 });
 
-MovieSchema.pre("save",function(next){
-	if(this.isNew){
+MovieSchema.pre("save", function(next) {
+	if (this.isNew) {
 		this.meta.createAt = this.meta.updateAt = Date.now();
-	}else{
+	} else {
 		this.meta.updateAt = Date.now();
 	}
 	next()
 });
 
 MovieSchema.statics = {
-	fetch:function(cb){
+	fetch: function(cb) {
 		return this
 			.find({})
 			.sort("meta.updateAt")
 			.exec(cb);
 	},
-	findById:function(id,cb){
+	findById: function(id, cb) {
 		return this
-			.findOne({_id:id})
+			.findOne({
+				_id: id
+			})
 			.exec(cb);
 	}
 }
---
+++ schemas/user.js
@@ -0,0 +1,59 @@
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
+var SALT_WORK_FACTOR = 10;
+
+var UserSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		unique: true
+	},
+	password: String,
+	meta: {
+		createAt: {
+			type: Date,
+			default: Date.now()
+		},
+		updateAt: {
+			type: Date,
+			default: Date.now()
+		}
+	}
+});
+
+UserSchema.pre("save", function(next) {
+	var user = this;
+	if (this.isNew) {
+		this.meta.createAt = this.meta.updateAt = Date.now();
+	} else {
+		this.meta.updateAt = Date.now();
+	}
+	bcrypt.genSalt(SALT_WORK_FACTOR,function(err,salt){
+		if(err) return next(err);
+		
+		bcrypt.hash(user.password,salt,function(err,hash){
+			if(err) return next(err);
+			
+			user.password = hash;
+			next();
+		});
+	});
+	next();
+});
+
+UserSchema.statics = {
+	fetch: function(cb) {
+		return this
+			.find({})
+			.sort("meta.updateAt")
+			.exec(cb);
+	},
+	findById: function(id, cb) {
+		return this
+			.findOne({
+				_id: id
+			})
+			.exec(cb);
+	}
+}
+
+module.exports = UserSchema;
